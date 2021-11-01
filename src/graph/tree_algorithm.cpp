f2ca9b7b1ad563e0f1134133c97d9e82ca0e4077
cyy <cyyever@outlook.com>
2021-11-01 15:59:39 +0800

    fix code

---
+++ src/graph/tree_algorithm.cpp
@@ -0,0 +1,32 @@
+/*!
+ * \file tree_algorithm.cpp
+ *
+ * \brief
+ */
+
+#include "tree_algorithm.hpp"
+namespace cyy::algorithm {
+  tree<size_t> recover_tree(const std::vector<size_t> &prufer_code) {
+    std::vector<edge<size_t>> edges;
+    std::set<size_t> vertices;
+    for (size_t v = 0; v < prufer_code.size() + 2; v++) {
+      if (std::ranges::find(prufer_code, v) == prufer_code.end()) {
+        vertices.insert(v);
+      }
+    }
+
+    for (auto it = prufer_code.begin(); it != prufer_code.end(); ++it) {
+      auto v = *it;
+      auto it2 = vertices.begin();
+      edges.emplace_back(*it2, v);
+      vertices.erase(it2);
+      if (std::find(it + 1, prufer_code.end(), v) == prufer_code.end()) {
+        vertices.insert(v);
+      }
+    }
+    assert(vertices.size() == 2);
+    edges.emplace_back(*vertices.begin(), *(vertices.rbegin()));
+
+    return tree<size_t>(std::move(edges));
+  }
+} // namespace cyy::algorithm
