7ba982758942de0c1e9790185a52f1dd1f558288
cyy <cyyever@outlook.com>
2021-11-24 14:08:36 +0000

    fix code

--- fuzz_test/helper.cpp
+++ fuzz_test/helper.cpp
@@ -12,9 +12,9 @@
 cyy::algorithm::graph<uint8_t> fuzzing_graph(const uint8_t *Data, size_t Size) {
   cyy::algorithm::graph<uint8_t> g;
   size_t i = 0;
-  while (i + 2 < Size)
+  while (i + 2 < Size) {
     g.add_edge({Data[i], Data[i + 1]});
-  i += 2;
-}
-return g;
+    i += 2;
+  }
+  return g;
 }
