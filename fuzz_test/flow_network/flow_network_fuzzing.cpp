2ce55b12b3b22457727c14865cb65c780fbae9a0
cyy <cyyever@outlook.com>
2021-11-30 13:23:18 +0800

    fix code

---
+++ fuzz_test/flow_network/flow_network_fuzzing.cpp
@@ -0,0 +1,15 @@
+/*!
+ * \file graph_fuzzing.cpp
+ *
+ */
+#include "../helper.hpp"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  try {
+    std::span data_span{Data,Size};
+    auto f = generate_flow_network(data_span);
+    f.max_flow_by_edmonds_karp();
+  } catch (const std::exception &) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
