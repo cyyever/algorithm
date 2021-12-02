88a2c08b028e0504a63b49044ad8f93f4349cfb9
cyy <cyyever@outlook.com>
2021-12-02 23:53:34 +0800

    add code

--- src/linear_programming/linear_program.hpp
+++ src/linear_programming/linear_program.hpp
@@ -17,9 +17,10 @@ namespace cyy::algorithm {
   template <typename number_type = double> class linear_program {
   public:
     using basis_type = polyhedron<number_type>::basis_type;
+    using matrix_type = polyhedron<number_type>::matrix_type;
+    using vector_type = polyhedron<number_type>::vector_type;
 
-    linear_program(cyy::math::la::vector<number_type> c_,
-                   polyhedron<number_type> A_b_)
+    linear_program(vector_type c_, polyhedron<number_type> A_b_)
         : c(std::move(c_)), A_b(std::move(A_b_)) {
       if (c.rows() != A_b.get_A().cols()) {
         throw std::invalid_argument("mismatched c and A_b");
@@ -33,12 +34,17 @@ namespace cyy::algorithm {
     bool is_primally_feasible_basis(const basis_type &basis) const {
       return A_b.is_feasible(A_b.get_extreme_point(basis));
     }
+    bool is_dually_feasible_basis(const basis_type &basis) const {
+      return c.dot(A_b.get_extreme_point(basis)) <= 0;
+    }
     std::optional<basis_type> primal_simplex_phase_2(const basis_type &basis) {
+      assert(is_primally_feasible_basis(basis));
       auto tableau = get_tableau_form(basis);
       while (true) {
-        auto y = tableau.topLeftCorner(1, get_A().cols());
-        auto it = std::ranges::find_if(y, [](auto const &a) { return a > 0; });
+        auto const &y = get_y(tableau);
+        auto it = std::ranges::find_if(y, [](auto const &n) { return n > 0; });
         if (it == y.end()) {
+          assert(is_primally_feasible_basis(basis));
           return basis;
         }
         auto pivot_col = std::advance(y.begin(), it);
@@ -50,10 +56,10 @@ namespace cyy::algorithm {
             continue;
           }
           if (min_lambda == -1) {
-            min_lambda = -row.tail(1) / row(pivot_col);
+            min_lambda = -row(Eigen::last) / row(pivot_col);
             pivot_row = row_idx;
           } else {
-            auto lambda = -row.tail(1) / row(pivot_col);
+            auto lambda = -row(Eigen::last) / row(pivot_col);
             if (lambda < min_lambda) {
               pivot_row = row_idx;
               min_lambda = lambda;
@@ -66,13 +72,66 @@ namespace cyy::algorithm {
         }
         basis.rease(pivot_col);
         basis.add(pivot_row);
-        tableau.cols(pivot_col) /= tableau(pivot_row, pivot_col);
-        for (int col_idx = 0; col_idx < tableau.cols(); col_idx++) {
-          tableau.col(col_idx) -=
-              tableau(pivot_row, col_idx) * tableau.col(pivot_col);
-        }
+        do_pivot_col_operation(tableau, pivot_row, pivot_col);
       }
+      return {};
+    }
 
+    std::optional<basis_type> dual_simplex_phase_2(const basis_type &basis) {
+      assert(is_dually_feasible_basis(basis));
+      auto tableau = get_tableau_form(basis);
+
+      while (true) {
+        auto b = tableau.topLeftCorner(1, get_A().cols());
+        auto it = std::ranges::find_if(b, [](auto const &n) { return n < 0; });
+        if (it == b.end()) {
+          assert(is_dually_feasible_basis(basis));
+          return basis;
+        }
+        auto pivot_row = std::advance(b.begin(), it);
+        number_type min_lambda = -1;
+        std::vector<int> pivot_cols;
+        for (auto col_idx = 0; col_idx + 1 < tableau.cols(); col_idx++) {
+          auto const &col = tableau.col(col_idx);
+          if (col(pivot_row) <= 0) {
+            continue;
+          }
+          if (min_lambda == -1) {
+            min_lambda = -col(0) / col(pivot_row);
+            pivot_cols.push_back(col_idx);
+          } else {
+            auto lambda = -col(0) / col(pivot_row);
+            if (lambda == min_lambda) {
+              pivot_cols.push_back(col_idx);
+            } else if (lambda < min_lambda) {
+              pivot_cols.clear();
+              pivot_cols.push_back(col_idx);
+              min_lambda = lambda;
+            }
+          }
+          assert(min_lambda >= 0);
+        }
+        if (min_lambda < 0) {
+          break;
+        }
+        // choose pivot_col from lexicographical order
+        auto pivot_col = *std::ranges::min_element(
+            pivot_cols, [](auto const &a, auto const &b) {
+              for (int i = 0; i < a.rows(); i++) {
+                if (a(i) < b(i)) {
+                  return true;
+                }
+                if (b(i) < a(i)) {
+                  return false;
+                }
+                return false;
+              }
+            });
+
+        basis.rease(pivot_row);
+        basis.add(pivot_col);
+        do_pivot_col_operation(tableau, pivot_row, pivot_col);
+      }
       return {};
     }
 
@@ -90,11 +149,27 @@ namespace cyy::algorithm {
     }
     auto const &get_A() const { return A_b.get_A(); }
     auto const &get_b() const { return A_b.get_b(); }
+    auto &get_b(matrix_type &tableau) const {
+      return tableau.bottomRightCorner(get_b().rows(), 1);
+    }
+
+    auto &get_y(matrix_type &tableau) const {
+      return tableau.topLeftCorner(1, get_A().cols());
+    }
+
+    static void do_pivot_col_operation(matrix_type &tableau, int pivot_row,
+                                       int pivot_col) {
+      tableau.cols(pivot_col) /= tableau(pivot_row, pivot_col);
+      for (int col_idx = 0; col_idx < tableau.cols(); col_idx++) {
+        tableau.col(col_idx) -=
+            tableau(pivot_row, col_idx) * tableau.col(pivot_col);
+      }
+    }
 
   private:
-    cyy::math::la::vector<number_type> c;
+    vector_type c;
     polyhedron<number_type> A_b;
-    std::optional<cyy::math::la::matrix<number_type>> kernel_space;
+    std::optional<matrix_type> kernel_space;
   };
 
 } // namespace cyy::algorithm
