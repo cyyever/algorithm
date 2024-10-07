f31db8448d63860b6fa33a90ac174238cfe275b8
cyy <cyyever@outlook.com>
2024-10-07 17:58:08 +0800

    Add code

--- test/trie_test.cpp
+++ test/trie_test.cpp
@@ -11,14 +11,26 @@
 using namespace cyy::algorithm;
 TEST_CASE("tire construction") {
   SUBCASE("string") {
-    cyy::algorithm::trie<char> string_trie(std::vector{std::string("abc"),std::string("abd")});
-    auto view= string_trie.get_level_view(0);
-    REQUIRE(std::ranges::size(view)==1);
-    for (auto a:view) {
-    REQUIRE(a==string_trie.get_data_id('a'));
-
+    cyy::algorithm::trie<char> string_trie(
+        std::vector{std::string("abc"), std::string("abd")});
+    auto view = string_trie.get_level_view(0);
+    REQUIRE(std::ranges::size(view) == 1);
+    for (auto a : view) {
+      REQUIRE(a.element_id == string_trie.get_data_id('a'));
     }
-
+    view = string_trie.get_level_view(1);
+    REQUIRE(std::ranges::size(view) == 1);
+    for (auto a : view) {
+      REQUIRE(a.parent_id == string_trie.get_data_id('a'));
+      REQUIRE(a.element_id == string_trie.get_data_id('b'));
+    }
+    view = string_trie.get_level_view(2);
+    REQUIRE(std::ranges::size(view) == 2);
+    for (auto a : view) {
+      REQUIRE(a.is_end);
+      auto res = (a.element_id == string_trie.get_data_id('c') ||
+                  a.element_id == string_trie.get_data_id('d'));
+      REQUIRE(res);
+    }
   }
-
 }
