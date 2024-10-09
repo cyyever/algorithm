6d6692dd530ca00cfe5e8366aa6f8e2eddabfa52
cyy <cyyever@outlook.com>
2024-10-10 00:53:28 +0800

    Fix code

--- test/ordered_dict_test.cpp
+++ test/ordered_dict_test.cpp
@@ -55,8 +55,6 @@ TEST_CASE("ordered_dict") {
     CHECK_EQ(k, 2);
     CHECK_EQ(v, "b");
     CHECK(container.empty());
-    container.pop_oldest();
-    CHECK_THROWS(container.pop_oldest());
     CHECK_THROWS(container.pop_oldest());
   }
 }
